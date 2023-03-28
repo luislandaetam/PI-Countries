f4c8cc5f3f01ce6d009d49ac1b8bd97ab808445f
Luis Landaeta <luis.landaeta24@gmail.com>
2023-03-28 13:50:12 -0400

    BaseURL change

--- client/src/index.js
+++ client/src/index.js
@@ -8,7 +8,7 @@ import { Provider } from "react-redux";
 import store from "./redux/store/store.js";
 import reportWebVitals from "./reportWebVitals";
 
-axios.defaults.baseURL = "http://localhost:3001";
+axios.defaults.baseURL = "https://pi-countries-production-680a.up.railway.app";
 
 ReactDOM.render(
   <React.StrictMode>
